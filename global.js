58cdc1c2b6785d8865ea0af22e0e902a1cf525b0
叫我小朋友 <alexchan1st@163.com>
2022-03-14 15:21:42 +0800

    init global

--- global.js
+++ global.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-let argv = process.argv;
+const { Command } = require('commander');
+const lib = require("./lib");
+const program = new Command();
+
+program
+    .name('ctf-util')
+    .description('CLI to some ctf utilities')
+    .version('0.1.0');
+
+program
+    .command("detect <Input>")
+    .action((Input) => {
+        console.log(`${Input} is likely to be encoded as:`);
+        console.log(`${lib.detect.detectAll(Input).join("\n")}`);
+    })
+
+program.parse();
