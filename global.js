79f72a6229360e5a23ee846783715e53d9b042de
叫我小朋友 <alexchan1st@163.com>
2023-02-07 17:33:19 +0800

    fix global error

--- global.js
+++ global.js
@@ -3,6 +3,7 @@
 const { Command } = require('commander');
 const lib = require("./lib");
 const program = new Command();
+const chalk = require('chalk');
 
 program
     .name('ctf-util')
@@ -12,8 +13,18 @@ program
 program
     .command("detect <Input>")
     .action((Input) => {
-        console.log(`${Input} is likely to be encoded as:`);
-        console.log(`${lib.detect.detectAll(Input).join("\n")}`);
+        console.log(chalk.bgBlue(Input), ` is likely to be encoded as:`);
+        let result = lib.detect.detectAll(Input);
+        console.log(chalk.yellow(`${Object.keys(result).reduce((prev, curr) => {
+            let val = result[curr];
+            if (result[curr] && typeof val === "boolean") {
+                prev.push(curr)
+            }
+            if (result[curr] && typeof val === "array" && val.length > 1) {
+                prev.push(curr)
+            }
+            return prev
+        }, []).join('\t') || "NULL"}`));
     })
 
 program.parse();
